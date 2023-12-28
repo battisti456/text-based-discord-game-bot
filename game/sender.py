70400708392cea72a5a3bb57d19a182e908a1c6f
battisti456 <battisti456@gmail.com>
2023-12-27 18:16:09 -0800

    fixed import in sender and removed sync_lock completely

--- game/sender.py
+++ game/sender.py
@@ -1,18 +1,18 @@
-from game import message
+from game.message import Message
 
 class Sender(object):
     #A callable object used to display a Message to player
     def __init__(self):
         pass
-    async def __call__(self,message:message.Message):
+    async def __call__(self,message:Message):
         return await self._send(message)
-    async def _send(self,message:message.Message):
+    async def _send(self,message:Message):
         pass
 class Multiple_Sender(Sender):
     def __init__(self,senders:list[Sender]):
         Sender.__init__(self)
         self.senders = senders
-    async def __call__(self, message: message.Message):
+    async def __call__(self, message: Message):
         for sender in self.senders:
             await sender(message)
         return await self._send(message)
