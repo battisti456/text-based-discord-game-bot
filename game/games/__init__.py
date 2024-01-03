a4e9a38da8e51008fb97236526ee966a1a341218
battisti456 <battisti456@gmail.com>
2024-01-02 23:13:25 -0800

    updated tgkr game to new syntax

--- game/games/__init__.py
+++ game/games/__init__.py
@@ -6,8 +6,7 @@ from game.games.tricky_trivia import Tricky_Trivia
 from game.games.guess_the_word import Guess_The_Word
 from game.games.elimination_rock_paper_scissors import Elimination_Rock_Paper_Scissors
 from game.games.container_bidding import Container_Bidding
-
-#from game.games.the_great_kitten_race import The_Great_Kitten_Race
+from game.games.the_great_kitten_race import The_Great_Kitten_Race
 #from game.games.prediction_texas_holdem import Prediction_Texas_Holdem
 #from game.games.chess_puzzle_elimination import Chess_Puzzle_Elimination
 #from game.games.altered_image_guess import Altered_Image_Guess
