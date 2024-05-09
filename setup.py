073c72ba3e5ef7e481c0b7cbade78b354ed1a893
battisti456 <battisti456@gmail.com>
2024-05-08 21:57:14 -0700

    added support for pip installing libraries neede only for setup during setup

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 import os
 import logging
 import nltk
+import subprocess
+import sys
 
 LICHESS_URL = "https://database.lichess.org/lichess_db_puzzle.csv.zst"
 LICHESS_PATH = "data/lichess_db_puzzle.csv"
@@ -9,6 +11,9 @@ NLTK_MODULES = [
     'brown'
 ]
 
+def pip_install(package:str):
+    subprocess.run([sys.executable,'-m','pip','install',package],check=True)
+
 setup_logger = logging.Logger("SETUP",level=logging.INFO)
 setup_logger.addHandler(logging.StreamHandler())
 
@@ -16,16 +21,17 @@ def setup():
     if not os.path.isfile(LICHESS_PATH):
         setup_logger.warning(f"File {LICHESS_PATH} not found!")
         setup_logger.info(f"Importing required libraries...")
-        import zstandard
+        pip_install('zstandard')#install only if needed, as not actually required for the main program
+        import zstandard#type:ignore
         from io import BytesIO
         from urllib.request import urlopen
         dctx = zstandard.ZstdDecompressor()
         setup_logger.info(f"Opening url {LICHESS_URL}...")
         url = urlopen(LICHESS_URL)
-        setup_logger.info(f"Opened url.")
+        setup_logger.info(f"Opened url. Downloading file...")
         in_stream = BytesIO(url.read())
         out_stream = open(LICHESS_PATH,'wb')
-        setup_logger.info("Opened in and out streams. Decompressing and writing...")
+        setup_logger.info("Downloaded file and opened in and out streams. Decompressing and writing...")
         dctx.copy_stream(in_stream,out_stream)
         out_stream.close()
         setup_logger.info(f"Successfully downloaded {LICHESS_PATH}.")
