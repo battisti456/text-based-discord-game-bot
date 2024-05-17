9de831826bd711fa964a668992ff7a5eb24c3839
Maggie <margaretmbattisti@gmail.com>
2024-05-17 16:48:03 -0700

    initialized probability_wager game

---
+++ game/games/probability_wager.py
@@ -0,0 +1,7 @@
+from game.game import Game, Game_Interface
+
+class Probability_Wager(Game):
+
+    def __init__(self, gi:Game_Interface) -> None:
+        super().__init__(gi)
+        #heyyyyyyyyyyyyyy
