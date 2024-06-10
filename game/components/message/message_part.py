9a7d6d6de7da2d4e8b0c90741f35e262744217de
battisti456 <battisti456@gmail.com>
2024-06-10 09:50:17 -0700

    new convenience methods for message parts

--- game/components/message/message_part.py
+++ game/components/message/message_part.py
@@ -1,14 +1,32 @@
 import dataclasses
 from typing import TYPE_CHECKING, Iterable, override
 
+from game import get_logger
+from utils.common import get_first
+
 if TYPE_CHECKING:
     from game.components.message.message import Message
 
+logger = get_logger(__name__)
+
+type MessagePartTypeSet = set[type['Message_Part']]
 
 @dataclasses.dataclass(frozen=True)
 class Message_Part():
     def bind(self,message:'Message'):
         message.add(self)
+    def comply_to(self,types:MessagePartTypeSet) -> set['Message_Part']:
+        ...
+    @classmethod
+    def alike_parts(cls,parts:set['Message_Part']) -> set['Message_Part']:
+        return set(part for part in parts if isinstance(part,cls))
+    @classmethod
+    def merge(cls,parts:set['Message_Part']) -> 'Message_Part':
+        alike = cls.alike_parts(parts)
+        to_return = get_first(alike)
+        if len(alike) != 1:
+            logger.warning(f"reducing {alike} to {to_return}")
+        return to_return
 
 def part_type(message_part:Message_Part|type[Message_Part]) -> type[Message_Part]:
     return message_part.__class__ if isinstance(message_part,Message_Part) else message_part
