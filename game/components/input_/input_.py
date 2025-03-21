1cd81bb962a30a68402c18bc5ceb670eeb1c4189
Autumn Battisti <battisti456@gmail.com>
2025-03-21 21:51:02 +0000

    added direct message flag to input reminders

--- game/components/input_/input_.py
+++ game/components/input_/input_.py
@@ -100,7 +100,8 @@ class Input(
                         clean_up.append(
                             await self.send(
                                 address = await self.sender.generate_address((participant,)),
-                                text=f"{mention_participants((participant,))}, we are still waiting for you to respond!" 
+                                text=f"{mention_participants((participant,))}, we are still waiting for you to respond!",
+                                with_flags=frozenset(('direct_message',))
                             )
                         )
                     try:
