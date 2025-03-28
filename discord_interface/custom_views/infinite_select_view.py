2deecf37e3ac69b15e969acb9c9de4fcdc3cc961
battisti456 <battisti456@gmail.com>
2025-03-28 15:57:03 -0700

    undid last, does not work

--- discord_interface/custom_views/infinite_select_view.py
+++ discord_interface/custom_views/infinite_select_view.py
@@ -20,7 +20,7 @@ logger = get_logger(__name__)
 if TYPE_CHECKING:
     from discord_interface.discord_interface import Discord_Game_Interface
 
-TIMEOUT = 30#in seconds
+TIMEOUT = 300#in seconds
 
 
 class _Infinite_Select_View(discord.ui.View):
@@ -200,7 +200,7 @@ class Infinite_View_Manager:
             )
             self.first = False
         else:
-            await discord_interaction.response.edit_message(content=text, view=view, delete_after=TIMEOUT)
+            await discord_interaction.response.edit_message(content=text, view=view)
 
 
 class Infinite_Select_Button(discord.ui.Button):
